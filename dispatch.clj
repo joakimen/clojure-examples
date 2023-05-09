74d264669817d6c9c0b1e96c50ce8b32e8540e27
Joakim L. Engeset <joakim.engeset@gmail.com>
2023-05-09 08:01:02 +0200

    Add example usage of cli/dispatch

---
+++ dispatch.clj
@@ -0,0 +1,29 @@
+(ns dispatch.main
+  "example of using babashka.cli/dispatch"
+  (:require [babashka.cli :as cli]
+            [clojure.string :as str]))
+
+(defn greet [{:keys [opts]}]
+  (println "Greetings," (:name opts)))
+
+(defn reverse [{:keys [opts]}]
+  (println (str/reverse (:name opts))))
+
+(defn help [_]
+  (println "help!"))
+
+(def subcommands
+  [{:cmds ["greet"] :fn greet :args->opts [:name]}
+   {:cmds ["reverse"] :fn reverse}
+   {:cmds [] :fn help}])
+
+(defn -main [& args]
+  (cli/dispatch subcommands args))
+
+(apply -main *command-line-args*)
+
+(comment
+
+  (apply -main ["greet" "kevin"])
+  ;;
+  )
